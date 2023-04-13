1be875cd4815ad007e3aab5a21c47b18e8e5d9d7
Kris Kruse <kriskruse@hotmail.com>
2023-04-13 10:32:30 +0200

    Data transfer base

--- ESP32_Master/ESP32_Master.ino
+++ ESP32_Master/ESP32_Master.ino
@@ -1,10 +1,69 @@
 
+#include <esp_now.h>
+#include <WiFi.h>
 
-void setup(){
+// Structure example to receive data
+// Must match the sender structure
+typedef struct SubData {
+  int id;
+  bool dist;
+  bool light;
+}SubData;
 
+// Create a SubData called myData
+SubData myData;
+
+// Create a structure to hold the readings from each board
+SubData board1;
+SubData board2;
+SubData board3;
+
+// Create an array with all the structures
+SubData boardsData[3] = {board1, board2, board3};
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsData[myData.id-1].dist = myData.dist;
+  boardsData[myData.id-1].light = myData.light;
+  Serial.printf("dist value: %d \n", boardsData[myData.id-1].dist);
+  Serial.printf("light value: %d \n", boardsData[myData.id-1].light);
+  Serial.println();
 }
+ 
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  // Acess the variables for each board
+  /*int board1X = boardsStruct[0].x;
+  int board1Y = boardsStruct[0].y;
+  int board2X = boardsStruct[1].x;
+  int board2Y = boardsStruct[1].y;
+  int board3X = boardsStruct[2].x;
+  int board3Y = boardsStruct[2].y;*/
 
-void loop(){
-
+  delay(10000);  
 }
--- SubBoards/SubBoards.ino
+++ SubBoards/SubBoards.ino
@@ -1,10 +1,73 @@
 
+#include <esp_now.h>
+#include <WiFi.h>
 
-void setup(){
+// REPLACE WITH THE RECEIVER'S MAC Address
+uint8_t broadcastAddress[] = {0x08, 0xB6, 0x1F, 0x81, 0x0F, 0xEC};
 
+// Structure example to send data
+// Must match the receiver structure
+typedef struct SubData {
+    int id; // must be unique for each sender board
+    bool dist;
+    bool light;
+} SubData;
+
+// Create a SubData called myData
+SubData myData;
+
+// Create peer interface
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
-void loop(){
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  // Set values to send
+  myData.id = 1;
+  myData.dist = random(0,1);
+  myData.light = random(0,1);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(10000);
 }
