204cbfe19ae98f086ffb8b7d0060e6fbd5776b78
Jordan Shopp <jordan.shopp123@gmail.com>
2023-05-01 16:23:07 +0200

    Does not work yet

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 
@@ -8,3 +9,310 @@ void setup(){
 void loop(){
     
 }
+=======
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/?s=esp-now
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  Based on JC Servaye example: https://github.com/Servayejc/esp_now_web_server/
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "AsyncTCP.h"
+#include <ArduinoJson.h>
+
+// Replace with your network credentials (STATION)
+const char *ssid = "KruseNet";
+const char *password = "Krusers47";
+
+esp_now_peer_info_t slave;
+int chan;
+
+enum MessageType { PAIRING,
+                   DATA,
+};
+MessageType messageType;
+
+int counter = 0;
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  uint8_t msgType;
+  uint8_t id;
+  int state;
+  bool reserved;
+  unsigned int readingId;
+} struct_message;
+
+typedef struct struct_pairing {  // new structure for pairing
+  uint8_t msgType;
+  uint8_t id;
+  uint8_t macAddr[6];
+  uint8_t channel;
+} struct_pairing;
+
+struct_message incomingReadings;
+struct_message outgoingSetpoints;
+struct_pairing pairingData;
+
+AsyncWebServer server(80);
+AsyncEventSource events("/events");
+
+const char index_html[] PROGMEM = R"rawliteral(<!DOCTYPE HTML><html>
+<head>
+  <title>ParkWise Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    p {  font-size: 1.2rem;}
+    body {  margin: 0;}
+    .topnav { overflow: hidden; background-color: #2f4468; color: white; font-size: 1.7rem; }
+    .content { padding: 20px; }
+    .card { background-color: white; box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); }
+    .cards { max-width: 700px; margin: 0 auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); }
+    .reading { font-size: 2.8rem; }
+    .packet { color: #bebebe; }
+    .card.state { color: #fd7e14; }
+    .card.reserved { color: #1b78e2; }
+  </style>
+</head>
+    
+<body>
+  <div class="topnav">
+    <h3>ParkWise Dashboard</h3>
+  </div>
+  <div class="content">
+    
+    <div style="font-size: 1.8rem;">Map</div>
+    <p></p>
+ 
+    
+    <div style="display: flex; justify-content: center;">
+      <!-- Below, change the colors so that they are correct --> 
+
+  <div style="background-color: red; color: black; font-size: 1.3rem; padding: 30px; margin-right: 10px;">Spot 1</div>
+      
+  <div style="background-color: yellow; padding: 30px; font-size: 1.3rem; color: black;">Spot 2</div>
+</div>
+    
+    <p></p>
+    <p>The map above illustrates the availability of parking spots. Red means that someone is parked there, yellow means someone reserved the spot, and green means the spot is available.</p>
+    
+    <div style="font-size: 1.8rem;">Reserve Here</div>
+    <p></p>
+    
+    <p>As soon as you reserve or park in a spot, the timer for your parking begins (you pay based on the timer). Click on the available spot button that you want to book.</p>
+        <!-- <p>This is another paragraph </p> --> 
+
+   <script>
+  window.onload = function() {
+    var x = 0; //x = spot1 state
+    if (x == 0) {
+      var btn1 = document.createElement("BUTTON");
+      var t1 = document.createTextNode("Book Spot One");
+      btn1.appendChild(t1);
+      btn1.style.fontSize = "16px"; 
+      btn1.style.padding = "12px"; 
+      btn1.onclick = function() {
+        window.location.href = "/spot_one";
+      }
+      document.body.appendChild(btn1);
+    }
+    var y = 0; //y=spot2 state
+    if (y == 0) {
+      var btn2 = document.createElement("BUTTON");
+      var t2 = document.createTextNode("Book Spot Two");
+      btn2.appendChild(t2);
+      btn2.style.fontSize = "16px"
+      btn2.style.padding = "12px";
+      btn2.onclick = function() {
+        window.location.href = "/spot_two";
+      }
+      document.body.appendChild(btn2);
+    }
+  }
+</script>
+
+
+    
+    
+    
+    
+</body>
+</html>)rawliteral";
+
+
+//Notes on String (String(colors[parkingStates[0]])), String(colors[parkingStates[1]]),
+void readDataToSend() {
+  outgoingSetpoints.msgType = DATA;
+  outgoingSetpoints.id = 0;
+  /* outgoingSetpoints.state = root["state"]; */
+  outgoingSetpoints.reserved = false;  // Get reserved state here
+  outgoingSetpoints.readingId = counter++;
+}
+
+
+// ---------------------------- esp_ now -------------------------
+void printMAC(const uint8_t *mac_addr) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+}
+
+bool addPeer(const uint8_t *peer_addr) {  // add pairing
+  memset(&slave, 0, sizeof(slave));
+  const esp_now_peer_info_t *peer = &slave;
+  memcpy(slave.peer_addr, peer_addr, 6);
+
+  slave.channel = chan;  // pick a channel
+  slave.encrypt = 0;     // no encryption
+  // check if the peer exists
+  bool exists = esp_now_is_peer_exist(slave.peer_addr);
+  if (exists) {
+    // Slave already paired.
+    Serial.println("Already Paired");
+    return true;
+  } else {
+    esp_err_t addStatus = esp_now_add_peer(peer);
+    if (addStatus == ESP_OK) {
+      // Pair success
+      Serial.println("Pair success");
+      return true;
+    } else {
+      Serial.println("Pair failed");
+      return false;
+    }
+  }
+}
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("Last Packet Send Status: ");
+  Serial.print(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success to " : "Delivery Fail to ");
+  printMAC(mac_addr);
+  Serial.println();
+}
+
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
+  Serial.print(len);
+  Serial.print(" bytes of data received from : ");
+  printMAC(mac_addr);
+  Serial.println();
+  StaticJsonDocument<1000> root;
+  String payload;
+  uint8_t type = incomingData[0];  // first message byte is the type of message
+  switch (type) {
+    case DATA:  // the message is data type
+      memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+      // create a JSON document with received data and send it by event to the web page
+      root["id"] = incomingReadings.id;
+      root["state"] = incomingReadings.state;
+      root["reserved"] = incomingReadings.reserved;
+      root["readingId"] = String(incomingReadings.readingId);
+      serializeJson(root, payload);
+      Serial.print("event send :");
+      serializeJson(root, Serial);
+      events.send(payload.c_str(), "new_readings", millis());
+      Serial.println();
+      break;
+
+    case PAIRING:  // the message is a pairing request
+      memcpy(&pairingData, incomingData, sizeof(pairingData));
+      Serial.println(pairingData.msgType);
+      Serial.println(pairingData.id);
+      Serial.print("Pairing request from: ");
+      printMAC(mac_addr);
+      Serial.println();
+      Serial.println(pairingData.channel);
+      if (pairingData.id > 0) {  // do not replay to server itself
+        if (pairingData.msgType == PAIRING) {
+          pairingData.id = 0;  // 0 is server
+          // Server is in AP_STA mode: peers need to send data to server soft AP MAC address
+          WiFi.softAPmacAddress(pairingData.macAddr);
+          pairingData.channel = chan;
+          Serial.println("send response");
+          esp_err_t result = esp_now_send(mac_addr, (uint8_t *)&pairingData, sizeof(pairingData));
+          addPeer(mac_addr);
+        }
+      }
+      break;
+  }
+}
+
+void initESP_NOW() {
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.print("Server MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  // Set the device as a Station and Soft Access Point simultaneously
+  WiFi.mode(WIFI_AP_STA);
+  // Set device as a Wi-Fi Station
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Setting as a Wi-Fi Station..");
+  }
+
+  Serial.print("Server SOFT AP MAC Address:  ");
+  Serial.println(WiFi.softAPmacAddress());
+
+  chan = WiFi.channel();
+  Serial.print("Station IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Wi-Fi Channel: ");
+  Serial.println(WiFi.channel());
+
+  initESP_NOW();
+
+  // Start Web server
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html);
+  });
+
+
+  // Events
+  events.onConnect([](AsyncEventSourceClient *client) {
+    if (client->lastId()) {
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  // start server
+  server.begin();
+}
+
+void loop() {
+  static unsigned long lastEventTime = millis();
+  static const unsigned long EVENT_INTERVAL_MS = 5000;
+  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+    events.send("ping", NULL, millis());
+    lastEventTime = millis();
+    readDataToSend();
+    esp_now_send(NULL, (uint8_t *)&outgoingSetpoints, sizeof(outgoingSetpoints));
+  }
+}
+>>>>>>> Stashed changes
--- Webserver_Parkify/Webserver_Parkify.ino
+++ Webserver_Parkify/Webserver_Parkify.ino
@@ -67,10 +67,81 @@ void setup() {
   Serial.println("Server started"); 
 }
 
+<<<<<<< Updated upstream
 void loop() {
   // Check if a client has connected
   server.handleClient();
 }
+=======
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            //button methods
+            if (header.indexOf("GET /spot0/on") >= 0) {
+              Serial.println("Spot0");
+              bookSpot(0);
+            } else if (header.indexOf("GET /spot0/off") >= 0) {
+              Serial.println("Spot0");
+              parkingStates[0]=0;
+            } else if (header.indexOf("GET /spot1/on") >= 0) {
+              Serial.println("Spot1");
+              bookSpot(1);
+            } else if (header.indexOf("GET /spot1/off") >= 0) {
+              Serial.println("Spot1");
+              parkingStates[0]=0;
+            } else if (header.indexOf("GET /cancel/on") >= 0) {
+              cancelBookings();
+              digitalWrite(output27, HIGH);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555; color: white;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Parkify Parking Lot</h1>");
+            client.println("<p>The map below illustrates the availability of parking spots.</p>");
+            client.println("<p>Red means that someone is parked there, yellow means someone reserved the spot, and green means the spot is available.</p>");
+
+            //Map
+            updateParkingStates();
+            client.println("<div style=\"display: flex; justify-content: center; height: 5vh; margin: 0 auto;\">");
+            client.println("  <div style=\"background-color: " + String(colors[parkingStates[0]]) + "; width: 100px; height: 50px; text-align: center; line-height: 50px; color: black;\">Spot 0</div>");
+            client.println("  <div style=\"background-color: " + String(colors[parkingStates[1]]) + "; width: 100px; height: 50px; text-align: center; line-height: 50px; color: black;\">Spot 1</div>");
+            client.println("</div>");
+            
+            client.println("<p>As soon as you reserve or park in a spot, the timer for your parking begins (you pay based on the timer).</p>");
+            client.println("<p>Click on the available spot button that you want to book.</p>");
+>>>>>>> Stashed changes
 
 
 /*void handleRoot() {                         // When URI / is requested, send a web page with a button to toggle the LED
